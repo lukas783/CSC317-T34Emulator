349d4a73e23fffc6a508decdbc927b5b8064abc9
Carpenter <Lucas.Carpenter@mines.sdsmt.edu>
2017-09-25 11:34:44 -0600

    Added loading of mem addresses and a memdump function

--- T34Emulator.cpp
+++ T34Emulator.cpp
@@ -2,12 +2,15 @@
 #include <fstream>
 #include <string.h>
 #include <string>
+#include <stdio.h>
 
 #define PROGNAME "T34Emulator.exe"
 #define MAXMEM 12288
 typedef unsigned char byte;
 
-void putWord(byte *memarray, char *memaddr, char *value);
+void putWord(byte *memarray, char *memaddr, char *value, int base = 16);
+char *getWord(byte *memarray, char *memaddr, int base = 16, int outbase = 10);
+void memdump(byte *memarray);
 
 int main(int argc, char* argv[]) {
     if(argc != 2) {
@@ -16,19 +19,46 @@ int main(int argc, char* argv[]) {
     }
     std::ifstream in;
     byte memarray[MAXMEM];
+    char mem[4] = "0c0";
+    char memval[8] = "200800";
     memset(memarray, 0, sizeof(memarray));
-    memarray[8] = 253;
-    for(int i = 0; i < MAXMEM/2; i+=3)
-        printf("[%d] - %d %d %d\n", i/3, memarray[i], memarray[i+1], memarray[i+2]);
-    
+    putWord(memarray, mem, memval);
+    memdump(memarray);
     return 0;
 }
 
-void putWord(byte *memarray, char *memaddr, char *value) {
+void readObj(char *filename, byte *memarray) {
+    
+}
+
+void putWord(byte *memarray, char *memaddr, char *value, int base) {
     int decAddr = std::stoi(memaddr, nullptr, 16)*3;
     int val = std::stoi(value, nullptr, 16);
     memarray[decAddr] = (val >> 16) & 0xFF;
     memarray[decAddr+1] = (val >> 8) & 0xFF;
     memarray[decAddr+2] = (val >> 0) & 0xFF;
-    printf("%u | %u | %u\n", memarray[decAddr], memarray[decAddr+1], memarray[decAddr+2]);
+}
+
+char* getWord(byte *memarray, char *memaddr, int base, int outbase) {
+    int decAddr = std::stoi(memaddr, nullptr, base)*3;
+    int retVal = 0;
+    retVal += (memarray[decAddr] << 16) | (memarray[decAddr+1] << 8) | (memarray[decAddr+2] << 0);
+    char *retStr = new char[26];
+    if(outbase == 16) 
+        sprintf(retStr, "%0x", retVal);
+    else if(outbase == 10)
+        sprintf(retStr, "%d", retVal);
+    else
+        printf("Invalid output base value, returning null.\n");
+    return retStr;
+}
+
+void memdump(byte *memarray) {
+    for(int i = 0; i < MAXMEM; i+=3) {
+        int val = 0;
+        val = (memarray[i] << 16) | (memarray[i+1] << 8) | (memarray[i+2] << 0);
+        if(val != 0) {
+            printf("[%03x] - %06x\n", (i/3), val);
+        }   
+    } 
 }
